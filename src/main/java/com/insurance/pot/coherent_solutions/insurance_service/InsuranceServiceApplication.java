cc97fd73ff7e01d886386ba276938227501cb6be
Edgar Milosh <edgar.milos@stud.vilniustech.lt>
2025-07-02 13:48:20 +0300

    Mentioned coherent solutions in project metadata Group

--- src/main/java/com/insurance/pot/coherent_solutions/insurance_service/InsuranceServiceApplication.java
+++ src/main/java/com/insurance/pot/coherent_solutions/insurance_service/InsuranceServiceApplication.java
@@ -1,4 +1,4 @@
-package com.insurance.pot.insurance_service;
+package com.insurance.pot.coherent_solutions.insurance_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
