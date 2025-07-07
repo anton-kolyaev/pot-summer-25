4b627716330de604d13aa4197c5ce713e62b59c8
Kasparas3 <kasparasmurmoka@gmail.com>
2025-07-07 17:11:31 +0300

    Update Address.java

--- src/main/java/com/coherentsolutions/pot/insurance_service/model/Address.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/model/Address.java
@@ -40,7 +40,4 @@ public class Address {
     // @ManyToMany(mappedBy = "addresses")
     // private List<Company> companies;
 
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
 }
