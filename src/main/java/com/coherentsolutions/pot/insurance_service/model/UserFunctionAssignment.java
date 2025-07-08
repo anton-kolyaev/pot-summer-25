1937bb3d98228e1745564572edd552480310decc
AgileAstapoviciute <148432756+AgileAstapoviciute@users.noreply.github.com>
2025-07-08 16:35:58 +0300

    LJT-5Replaced the wildcard import with explicit imports from

--- src/main/java/com/coherentsolutions/pot/insurance_service/model/UserFunctionAssignment.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/model/UserFunctionAssignment.java
@@ -1,6 +1,16 @@
 package com.coherentsolutions.pot.insurance_service.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
