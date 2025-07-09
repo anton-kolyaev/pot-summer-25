f75c1c4c05732d6b2dcffc80579f7dba0c5afbb0
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-09 17:11:09 +0300

    Add phone table script

--- src/test/java/com/coherentsolutions/pot/insurance_service/InsuranceServiceApplicationTests.java
+++ src/test/java/com/coherentsolutions/pot/insurance_service/InsuranceServiceApplicationTests.java
@@ -1,8 +1,10 @@
 package com.coherentsolutions.pot.insurance_service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class InsuranceServiceApplicationTests {
 
