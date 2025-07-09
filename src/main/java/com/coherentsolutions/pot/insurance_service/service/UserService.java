53120ffd7f2048f4e0c98d5fe3158a539ecf50b0
AgileAstapoviciute <148432756+AgileAstapoviciute@users.noreply.github.com>
2025-07-09 10:03:31 +0300

    Created UserService (initial commit)

---
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/UserService.java
@@ -0,0 +1,69 @@
+package com.coherentsolutions.pot.insurance_service.service;
+
+import org.springframework.stereotype.Service;
+import com.coherentsolutions.pot.insurance_service.model.User;
+import com.coherentsolutions.pot.insurance_service.repository.UserRepository;
+import com.coherentsolutions.pot.insurance_service.dto.user.UserRequestDto;
+import com.coherentsolutions.pot.insurance_service.dto.user.UserResponseDto;
+import com.coherentsolutions.pot.insurance_service.model.UserFunctionAssignment;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+
+    private List<UserRequestDto> getAllUserRequestDtos() {
+        return userRepository.findAll()
+                .stream()
+                .map(this::convertEntityToRequestDto)
+                .collect(Collectors.toList());
+    }
+
+    private UserRequestDto convertEntityToRequestDto(User user) {
+        UserRequestDto userRequestDto = new UserRequestDto();
+        userRequestDto.setFirstName(user.getFirstName());
+        userRequestDto.setLastName(user.getLastName());
+        userRequestDto.setUserName(user.getUsername());
+        userRequestDto.setEmail(user.getEmail());
+        userRequestDto.setDateOfBirth(user.getDateOfBirth());
+        userRequestDto.setSsn(user.getSsn());
+        userRequestDto.setFunctions(
+            user.getFunctions()
+                .stream()
+                .map(UserFunctionAssignment::getFunction)
+                .collect(Collectors.toList())
+        );
+        userRequestDto.setStatus(user.getStatus());
+        //userRequestDto.setCompanyId(user.getCompanyId());
+        return userRequestDto;
+    }
+
+    public List<UserResponseDto> getAllResponseDtos() {
+        return userRepository.findAll()
+                .stream()
+                .map(this::convertEntityToResponseDto)
+                .collect(Collectors.toList());
+    }
+
+    private UserResponseDto convertEntityToResponseDto(User user) {
+        UserResponseDto userResponseDto = new UserResponseDto();
+        userResponseDto.setFirstName(user.getFirstName());
+        userResponseDto.setLastName(user.getLastName());
+        userResponseDto.setUserName(user.getUsername());
+        userResponseDto.setEmail(user.getEmail());
+        userResponseDto.setDateOfBirth(user.getDateOfBirth());
+        userResponseDto.setSsn(user.getSsn());
+        userResponseDto.setFunctions(
+            user.getFunctions()
+                .stream()
+                .map(UserFunctionAssignment::getFunction)
+                .collect(Collectors.toList())
+        );
+        userResponseDto.setStatus(user.getStatus());
+        //userResponseDto.setCompanyId(user.getCompanyId());
+        return userResponseDto;
+    }
+
+}
