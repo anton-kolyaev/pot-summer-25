9e36ff70f2b93dafcbebcc57b0417f7a9e5ab1e5
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-11 00:28:14 +0300

    Add service into controller

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -1,10 +1,16 @@
 package com.coherentsolutions.pot.insurance_service.controller;
 
+import com.coherentsolutions.pot.insurance_service.service.CompanyManagementService;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class AdminCompanyManagementController {
+    private final CompanyManagementService companyManagementService;
+
+    public AdminCompanyManagementController(CompanyManagementService companyManagementService) {
+        this.companyManagementService = companyManagementService;
+    }
 
 
 }
