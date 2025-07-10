b8bd34f02bb18104ab3dc3d133c76337aab483a4
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-11 01:05:22 +0300

    LJT44-POST/companies

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -1,6 +1,12 @@
 package com.coherentsolutions.pot.insurance_service.controller;
 
+import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyRequest;
+import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyResponse;
 import com.coherentsolutions.pot.insurance_service.service.CompanyManagementService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,5 +18,10 @@ public class AdminCompanyManagementController {
         this.companyManagementService = companyManagementService;
     }
 
+    @PostMapping("/companies")
+    public ResponseEntity<CreateCompanyResponse> addCompany(@RequestBody CreateCompanyRequest companyDto) {
+        CreateCompanyResponse response = companyManagementService.createCompany(companyDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
@@ -1,13 +1,32 @@
 package com.coherentsolutions.pot.insurance_service.service;
 
+import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyRequest;
+import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyResponse;
+import com.coherentsolutions.pot.insurance_service.mapper.CompanyMapper;
+import com.coherentsolutions.pot.insurance_service.model.Company;
 import com.coherentsolutions.pot.insurance_service.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class CompanyManagementService {
     private final CompanyRepository companyRepository;
-    public CompanyManagementService(CompanyRepository companyRepository) {
+    private final CompanyMapper companyMapper;
+
+    public CompanyManagementService(CompanyRepository companyRepository, CompanyMapper companyMapper) {
         this.companyRepository = companyRepository;
+        this.companyMapper = companyMapper;
+    }
+
+    public CreateCompanyResponse createCompany(CreateCompanyRequest companyDto) {
+        Company company = companyMapper.toEntity(companyDto);
+        company.setAddressData(companyMapper.convertAddressListToMap(companyDto.getAddressData()));
+        company.setPhoneData(companyMapper.convertPhoneListToMap(companyDto.getPhoneData()));
+        company.setCreatedBy(companyDto.getCreatedBy());
+
+        companyRepository.save(company);
+
+        return companyMapper.toCreateCompanyResponse(company);
     }
 
 }
