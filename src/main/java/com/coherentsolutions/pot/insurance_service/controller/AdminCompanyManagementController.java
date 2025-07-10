cb0679f4ff31db1aa2b8203a1df8cf29c07f4317
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-11 00:08:44 +0300

    Add empty controller and service

---
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -0,0 +1,10 @@
+package com.coherentsolutions.pot.insurance_service.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AdminCompanyManagementController {
+
+
+}
---
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
@@ -0,0 +1,8 @@
+package com.coherentsolutions.pot.insurance_service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompanyManagementService {
+
+}
