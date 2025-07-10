039f011ac66dd5b9c722259da4cd109181e8cef5
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-11 01:40:36 +0300

    LJT44-GET/companies/id

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -1,7 +1,15 @@
 package com.coherentsolutions.pot.insurance_service.controller;
 
+
 import com.coherentsolutions.pot.insurance_service.service.CompanyManagementService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.coherentsolutions.pot.insurance_service.dto.CompanyDetailsResponse;
+
+
+import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
@@ -10,6 +18,13 @@ public class AdminCompanyManagementController {
 
     public AdminCompanyManagementController(CompanyManagementService companyManagementService) {
         this.companyManagementService = companyManagementService;
+    }
+
+
+    @GetMapping("/companies/{id}")
+    public Optional<CompanyDetailsResponse> viewCompanyDetails(@PathVariable String id)
+    {
+        return companyManagementService.getCompanyDetails(UUID.fromString(id));
     }
 
 
--- src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
@@ -1,13 +1,32 @@
 package com.coherentsolutions.pot.insurance_service.service;
 
+
+import com.coherentsolutions.pot.insurance_service.dto.CompanyDetailsResponse;
+import com.coherentsolutions.pot.insurance_service.mapper.CompanyMapper;
+import com.coherentsolutions.pot.insurance_service.model.Company;
 import com.coherentsolutions.pot.insurance_service.repository.CompanyRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CompanyManagementService {
     private final CompanyRepository companyRepository;
-    public CompanyManagementService(CompanyRepository companyRepository) {
+    private final CompanyMapper companyMapper;
+
+    public CompanyManagementService(CompanyRepository companyRepository, CompanyMapper companyMapper) {
         this.companyRepository = companyRepository;
+        this.companyMapper = companyMapper;
     }
+
+    public Optional<CompanyDetailsResponse> getCompanyDetails(UUID id) {
+        Company company = companyRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found"));
+        return Optional.of(companyMapper.toCompanyDetailsResponse(company));
+    }
+
 
 }
