25af60c555091c44c56a3724621c32724f01600e
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-10 22:32:13 +0300

    Add user entity

---
+++ src/main/java/com/coherentsolutions/pot/insurance_service/model/UserFunctionAssignment.java
@@ -0,0 +1,37 @@
+package com.coherentsolutions.pot.insurance_service.model;
+
+import com.coherentsolutions.pot.insurance_service.enums.UserFunction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user_functions")
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserFunctionAssignment {
+    
+    @Id
+    @GeneratedValue
+    private Integer id;  
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserFunction function;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
