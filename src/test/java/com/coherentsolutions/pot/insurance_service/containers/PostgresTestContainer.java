81b19640311393c4411bf0fbe8f3c961a59e68c2
Mark <mark.andzejevski01@gmail.com>
2025-07-10 10:53:44 +0300

    Add PostgreSQL Testcontainer support for integration tests

---
+++ src/test/java/com/coherentsolutions/pot/insurance_service/containers/PostgresTestContainer.java
@@ -0,0 +1,25 @@
+package com.coherentsolutions.pot.insurance_service.containers;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public abstract class PostgresTestContainer {
+
+    @Container
+    public static final PostgreSQLContainer<?> POSTGRES =
+            new PostgreSQLContainer<>("postgres:16.9")
+                    .withDatabaseName("insurance_service")
+                    .withUsername("insurance_app")
+                    .withPassword("insurance_app_password");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+}
