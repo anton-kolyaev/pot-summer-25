c09619814fa98304e1ccd97810db7dc6cde4789d
Mark <mark.andzejevski01@gmail.com>
2025-07-10 12:38:23 +0300

    Minor change

--- src/test/java/com/coherentsolutions/pot/insurance_service/InsuranceServiceApplicationTests.java
+++ src/test/java/com/coherentsolutions/pot/insurance_service/InsuranceServiceApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-
 class InsuranceServiceApplicationTests extends PostgresTestContainer {
 
 	@Test
