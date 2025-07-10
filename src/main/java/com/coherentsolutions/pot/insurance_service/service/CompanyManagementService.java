d6b43eafa67c24120a7675e800d8adc40f80be9c
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-11 00:15:58 +0300

    Add repositoru inside service

--- src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
@@ -1,8 +1,13 @@
 package com.coherentsolutions.pot.insurance_service.service;
 
+import com.coherentsolutions.pot.insurance_service.repository.CompanyRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyManagementService {
+    private final CompanyRepository companyRepository;
+    public CompanyManagementService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
 
 }
