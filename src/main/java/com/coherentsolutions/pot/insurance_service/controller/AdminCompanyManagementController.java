44376061a7bead1ff8d86d29d8d32296f367702c
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-11 15:40:29 +0300

    Add @Request mapping on the top of controller, add required imports

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -5,13 +5,9 @@ import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyRequest;
 import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyResponse;
 import com.coherentsolutions.pot.insurance_service.service.CompanyManagementService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import org.springframework.web.bind.annotation.RestController;
 import com.coherentsolutions.pot.insurance_service.dto.CompanyDetailsResponse;
 
 
@@ -21,17 +17,18 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/companies")
 public class AdminCompanyManagementController {
     private final CompanyManagementService companyManagementService;
 
 
-    @PostMapping("/companies")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CreateCompanyResponse addCompany(@RequestBody CreateCompanyRequest companyDto) {
         return companyManagementService.createCompany(companyDto);
     }
 
-    @GetMapping("/companies/{id}")
+    @GetMapping("/{id}")
     public CompanyDetailsResponse viewCompanyDetails(@PathVariable UUID id) {
         return companyManagementService.getCompanyDetails(id);
 
