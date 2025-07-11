920c52f7f64cfa82bb2b08876df886424bf80e5d
Tishkin <mykyta.tishkin@outlook.com>
2025-07-11 18:32:29 +0300

    removed constructor with @RequiredArgsConstructor

--- src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
@@ -8,22 +8,17 @@ import com.coherentsolutions.pot.insurance_service.model.Company;
 import com.coherentsolutions.pot.insurance_service.repository.CompanyRepository;
 import com.coherentsolutions.pot.insurance_service.repository.CompanySpecification;
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
 import java.util.List;
 import java.util.stream.Collectors;
 
 
 
 @Service
+@RequiredArgsConstructor
 public class CompanyManagementService {
     private final CompanyRepository companyRepository;
     private final CompanyMapper companyMapper;
-
-    public CompanyManagementService(
-            CompanyRepository companyRepository, 
-            CompanyMapper companyMapper) {
-        this.companyRepository = companyRepository;
-        this.companyMapper = companyMapper;
-    }
 
     public List<CompanyResponseDto> getCompaniesWithFilters(CompanyFilter filter) {
         // Use JPA Specification to filter at database level
