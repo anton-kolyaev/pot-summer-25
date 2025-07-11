d82d697cf0e08b5328c70d6756feefbe8f1106c6
AgileAstapoviciute <148432756+AgileAstapoviciute@users.noreply.github.com>
2025-07-11 08:36:13 +0300

    LJT-56 Create User Dto

---
+++ src/main/java/com/coherentsolutions/pot/insurance_service/dto/user/UserDto.java
@@ -0,0 +1,31 @@
+package com.coherentsolutions.pot.insurance_service.dto.user;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+import com.coherentsolutions.pot.insurance_service.enums.UserFunction;
+import com.coherentsolutions.pot.insurance_service.enums.UserStatus;
+import com.coherentsolutions.pot.insurance_service.dto.AddressDto;
+import com.coherentsolutions.pot.insurance_service.dto.PhoneDto;
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserDto {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private LocalDate dateOfBirth;
+    private String ssn;
+    private List<AddressDto> address_data;
+    private List<PhoneDto> phone_data;
+    private List<UserFunction> functions;
+    private UserStatus status;
+    private UUID companyId; 
+}
