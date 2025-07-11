7bdddf96e51dd5ef58e0f3a2e16e341c4452d559
Mark <mark.andzejevski01@gmail.com>
2025-07-11 17:34:37 +0300

    Deleted import duplication

--- src/main/java/com/coherentsolutions/pot/insurance_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/exception/GlobalExceptionHandler.java
@@ -17,7 +17,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import java.time.Instant;
 import java.util.HashMap;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
