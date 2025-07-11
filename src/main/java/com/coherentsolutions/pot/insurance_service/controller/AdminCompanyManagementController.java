2a9adb966280ff3f11a6f7a41e5334d4311fa06e
Edgar Miloš <117413271+EdgarMilosh@users.noreply.github.com>
2025-07-11 15:00:16 +0300

    LJT44 #3 GET/companies/{id} (#49)
    * LJT44-GET/companies/id
    
    * Add @RequiredArgsConstructor to Controller and service
    
    * Remove Optional from service and Controller
    
    * Replace type in controller Pathvariable: from String to UUID

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -1,16 +1,26 @@
 package com.coherentsolutions.pot.insurance_service.controller;
 
+
 import com.coherentsolutions.pot.insurance_service.service.CompanyManagementService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import com.coherentsolutions.pot.insurance_service.dto.CompanyDetailsResponse;
+
+
+import java.util.Optional;
+import java.util.UUID;
 
 
 @RestController
+@RequiredArgsConstructor
 public class AdminCompanyManagementController {
     private final CompanyManagementService companyManagementService;
 
-    public AdminCompanyManagementController(CompanyManagementService companyManagementService) {
-        this.companyManagementService = companyManagementService;
+    @GetMapping("/companies/{id}")
+    public CompanyDetailsResponse viewCompanyDetails(@PathVariable UUID id) {
+        return companyManagementService.getCompanyDetails(id);
     }
-
 
 }
--- src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
@@ -1,13 +1,29 @@
 package com.coherentsolutions.pot.insurance_service.service;
 
+
+import com.coherentsolutions.pot.insurance_service.dto.CompanyDetailsResponse;
+import com.coherentsolutions.pot.insurance_service.mapper.CompanyMapper;
+import com.coherentsolutions.pot.insurance_service.model.Company;
 import com.coherentsolutions.pot.insurance_service.repository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class CompanyManagementService {
     private final CompanyRepository companyRepository;
-    public CompanyManagementService(CompanyRepository companyRepository) {
-        this.companyRepository = companyRepository;
+    private final CompanyMapper companyMapper;
+
+    public CompanyDetailsResponse getCompanyDetails(UUID id) {
+        Company company = companyRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found"));
+        return companyMapper.toCompanyDetailsResponse(company);
     }
+
 
 }
