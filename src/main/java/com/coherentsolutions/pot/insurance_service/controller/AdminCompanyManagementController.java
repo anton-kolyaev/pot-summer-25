39de3d9459d7ec49d21b8103ddc3ecc9562c58e5
Tishkin <mykyta.tishkin@outlook.com>
2025-07-11 16:56:00 +0300

    New mapping in Controller

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -3,20 +3,22 @@ package com.coherentsolutions.pot.insurance_service.controller;
 import com.coherentsolutions.pot.insurance_service.dto.CompanyResponseDto;
 import com.coherentsolutions.pot.insurance_service.dto.CompanyFilter;
 import com.coherentsolutions.pot.insurance_service.service.CompanyManagementService;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/companies")
 public class AdminCompanyManagementController {
     private final CompanyManagementService companyManagementService;
 
-    public AdminCompanyManagementController(CompanyManagementService companyManagementService) {
-        this.companyManagementService = companyManagementService;
-    }
-
-    @GetMapping("/companies")
+    @GetMapping
     public List<CompanyResponseDto> getCompanies(CompanyFilter filter) {
         return companyManagementService.getCompaniesWithFilters(filter);
     }
