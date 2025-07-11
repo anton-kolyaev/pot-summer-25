2d236808275fe75b88c909c3088743bb117cf2bf
Edgar Miloš <117413271+EdgarMilosh@users.noreply.github.com>
2025-07-11 15:49:32 +0300

    LJT44 #1 POST/companies (#47)
    * LJT44-POST/companies
    
    * Remove ResponseEntity wrapping, add @RequiredArgsConstructor
    
    * Remove line, setting createdBy field mannualy
    
    * Restore missing brackets
    
    * Update POST/companies, add ResponseStatus:Created
    
    Co-authored-by: Alexander Novash <alexandernovash@gmail.com>
    
    * Add @Request mapping on the top of controller, add required imports
    
    ---------
    
    Co-authored-by: Alexander Novash <alexandernovash@gmail.com>

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -1,11 +1,13 @@
 package com.coherentsolutions.pot.insurance_service.controller;
 
 
+import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyRequest;
+import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyResponse;
 import com.coherentsolutions.pot.insurance_service.service.CompanyManagementService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
 import com.coherentsolutions.pot.insurance_service.dto.CompanyDetailsResponse;
 
 
@@ -15,12 +17,21 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/companies")
 public class AdminCompanyManagementController {
     private final CompanyManagementService companyManagementService;
 
-    @GetMapping("/companies/{id}")
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CreateCompanyResponse addCompany(@RequestBody CreateCompanyRequest companyDto) {
+        return companyManagementService.createCompany(companyDto);
+    }
+
+    @GetMapping("/{id}")
     public CompanyDetailsResponse viewCompanyDetails(@PathVariable UUID id) {
         return companyManagementService.getCompanyDetails(id);
+
     }
 
 }
--- src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
@@ -1,17 +1,24 @@
 package com.coherentsolutions.pot.insurance_service.service;
 
 
+import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyRequest;
+import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyResponse;
+
 import com.coherentsolutions.pot.insurance_service.dto.CompanyDetailsResponse;
+
 import com.coherentsolutions.pot.insurance_service.mapper.CompanyMapper;
 import com.coherentsolutions.pot.insurance_service.model.Company;
 import com.coherentsolutions.pot.insurance_service.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 import java.util.UUID;
+
 
 @Service
 @RequiredArgsConstructor
@@ -19,10 +26,22 @@ public class CompanyManagementService {
     private final CompanyRepository companyRepository;
     private final CompanyMapper companyMapper;
 
+
+    public CreateCompanyResponse createCompany(CreateCompanyRequest companyDto) {
+        Company company = companyMapper.toEntity(companyDto);
+        company.setAddressData(companyMapper.convertAddressListToMap(companyDto.getAddressData()));
+        company.setPhoneData(companyMapper.convertPhoneListToMap(companyDto.getPhoneData()));
+
+        companyRepository.save(company);
+
+        return companyMapper.toCreateCompanyResponse(company);
+    }
+
     public CompanyDetailsResponse getCompanyDetails(UUID id) {
         Company company = companyRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Company not found"));
         return companyMapper.toCompanyDetailsResponse(company);
+
     }
 
 
