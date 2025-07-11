ddd1dc8758ba8ed858790c968e18a41f51108db9
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-11 12:42:28 +0300

    Remove ResponseEntity wrapping, add @RequiredArgsConstructor

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminCompanyManagementController.java
@@ -3,25 +3,20 @@ package com.coherentsolutions.pot.insurance_service.controller;
 import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyRequest;
 import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyResponse;
 import com.coherentsolutions.pot.insurance_service.service.CompanyManagementService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequiredArgsConstructor
 public class AdminCompanyManagementController {
     private final CompanyManagementService companyManagementService;
 
-    public AdminCompanyManagementController(CompanyManagementService companyManagementService) {
-        this.companyManagementService = companyManagementService;
-    }
-
     @PostMapping("/companies")
-    public ResponseEntity<CreateCompanyResponse> addCompany(@RequestBody CreateCompanyRequest companyDto) {
-        CreateCompanyResponse response = companyManagementService.createCompany(companyDto);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    public CreateCompanyResponse addCompany(@RequestBody CreateCompanyRequest companyDto) {
+        return companyManagementService.createCompany(companyDto);
     }
 
 }
--- src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/CompanyManagementService.java
@@ -5,18 +5,15 @@ import com.coherentsolutions.pot.insurance_service.dto.CreateCompanyResponse;
 import com.coherentsolutions.pot.insurance_service.mapper.CompanyMapper;
 import com.coherentsolutions.pot.insurance_service.model.Company;
 import com.coherentsolutions.pot.insurance_service.repository.CompanyRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@RequiredArgsConstructor
 public class CompanyManagementService {
     private final CompanyRepository companyRepository;
     private final CompanyMapper companyMapper;
-
-    public CompanyManagementService(CompanyRepository companyRepository, CompanyMapper companyMapper) {
-        this.companyRepository = companyRepository;
-        this.companyMapper = companyMapper;
-    }
 
     public CreateCompanyResponse createCompany(CreateCompanyRequest companyDto) {
         Company company = companyMapper.toEntity(companyDto);
