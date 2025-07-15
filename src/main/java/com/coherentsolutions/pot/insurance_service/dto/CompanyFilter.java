ea4402489f047dc0839fcfa602ae887251cbac97
Tishkin <mykyta.tishkin@outlook.com>
2025-07-15 10:47:58 +0200

    NoArgsContstructor in CompanyFilter

--- src/main/java/com/coherentsolutions/pot/insurance_service/dto/CompanyFilter.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/dto/CompanyFilter.java
@@ -1,7 +1,7 @@
 package com.coherentsolutions.pot.insurance_service.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
@@ -9,7 +9,7 @@ import java.time.Instant;
 
 @Setter
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class CompanyFilter {
     private String name;
     private String countryCode;
@@ -18,8 +18,4 @@ public class CompanyFilter {
     private Instant createdTo;
     private Instant updatedFrom;
     private Instant updatedTo;
-
-    // Default constructor
-    public CompanyFilter() {}
-
 }
