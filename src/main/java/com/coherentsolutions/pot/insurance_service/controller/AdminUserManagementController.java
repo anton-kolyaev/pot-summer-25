cd804ab819537fef2a292c82419ebb7dee50caaa
AgileAstapoviciute <148432756+AgileAstapoviciute@users.noreply.github.com>
2025-07-15 14:08:50 +0300

    Using filter obj as an input parameter

--- src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminUserManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/controller/AdminUserManagementController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.coherentsolutions.pot.insurance_service.dto.user.UserDto;
 import com.coherentsolutions.pot.insurance_service.dto.user.UserFilter;
@@ -21,15 +22,13 @@ import com.coherentsolutions.pot.insurance_service.enums.UserStatus;
 import com.coherentsolutions.pot.insurance_service.service.UserManagementService;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/users")
 public class AdminUserManagementController {
     private final UserManagementService userManagementService;
-
-    public AdminUserManagementController(UserManagementService userManagementService) {
-        this.userManagementService = userManagementService;
-    }
   
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -38,17 +37,8 @@ public class AdminUserManagementController {
     }
 
     @GetMapping
-    public Page<UserDto> getUsersWithFilters(
-    @RequestParam(required = false) String name,
-    @RequestParam(required = false) String email,
-    @RequestParam(required = false) LocalDate dateOfBirth,
-    @RequestParam(required = false) UserStatus status,
-    @RequestParam(required = false) String ssn,
-    @RequestParam(required = false) Set<UserFunction> functions,
-    Pageable pageable
-    ) {
-    UserFilter filter = new UserFilter(name, email, dateOfBirth, status, ssn, functions);
-    return userManagementService.getUsersWithFilters(filter, pageable);
-    }  
+    public Page<UserDto> getUsersWithFilters (UserFilter filter, Pageable pageable) {
+        return userManagementService.getUsersWithFilters(filter, pageable);
+    }
 
 }
--- src/main/java/com/coherentsolutions/pot/insurance_service/model/User.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/model/User.java
@@ -20,6 +20,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -82,7 +83,7 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserStatus status;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
     private Set<UserFunctionAssignment> functions;
 
     @CreatedBy
