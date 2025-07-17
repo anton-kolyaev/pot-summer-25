d960aef034bf2ab520f614f8e1ade61c29095b60
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-17 14:56:24 +0300

    Add function updates

--- src/main/java/com/coherentsolutions/pot/insurance_service/service/UserManagementService.java
+++ src/main/java/com/coherentsolutions/pot/insurance_service/service/UserManagementService.java
@@ -1,6 +1,8 @@
 package com.coherentsolutions.pot.insurance_service.service;
 
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.coherentsolutions.pot.insurance_service.dto.user.UserDto;
 import com.coherentsolutions.pot.insurance_service.dto.user.UserFilter;
+import com.coherentsolutions.pot.insurance_service.enums.UserFunction;
 import com.coherentsolutions.pot.insurance_service.mapper.UserMapper;
 import com.coherentsolutions.pot.insurance_service.model.User;
 import com.coherentsolutions.pot.insurance_service.model.UserFunctionAssignment;
@@ -55,7 +58,30 @@ public class UserManagementService {
         user.setPhoneData(request.getPhoneData());
         user.setAddressData(request.getAddressData());
 
+        if (request.getFunctions() != null) {
+            Set<UserFunction> incomingFunctions = request.getFunctions();
+            Set<UserFunctionAssignment> currentAssignments = user.getFunctions();
+
+            currentAssignments.removeIf(assignment -> !incomingFunctions.contains(assignment.getFunction()));
+
+            Set<UserFunction> currentFunctions = currentAssignments.stream()
+                    .map(UserFunctionAssignment::getFunction)
+                    .collect(Collectors.toSet());
+
+            for (UserFunction function : incomingFunctions) {
+                if (!currentFunctions.contains(function)) {
+                    UserFunctionAssignment newAssignment = new UserFunctionAssignment();
+                    newAssignment.setFunction(function);
+                    newAssignment.setUser(user);
+                    currentAssignments.add(newAssignment);
+                }
+            }
+
+            user.setFunctions(currentAssignments);
+        }
+
         User updated = userRepository.save(user);
         return userMapper.toDto(updated);
+
     }
 }
