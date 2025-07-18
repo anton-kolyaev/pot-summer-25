42acfbac449f79dfc1fe7dfd2c9d9c4cb1f29ca9
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-18 17:29:15 +0300

    Add missing imports

--- src/test/java/com/coherentsolutions/pot/insurance_service/unit/service/UserManagementServiceTest.java
+++ src/test/java/com/coherentsolutions/pot/insurance_service/unit/service/UserManagementServiceTest.java
@@ -32,6 +32,10 @@ import com.coherentsolutions.pot.insurance_service.model.User;
 import com.coherentsolutions.pot.insurance_service.repository.UserRepository;
 import com.coherentsolutions.pot.insurance_service.service.UserManagementService;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class UserManagementServiceTest {
     @Mock
     private UserRepository userRepository;
