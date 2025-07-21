48953192cf740b8018cc24be47faf59cdc7e0451
AgileAstapoviciute <148432756+AgileAstapoviciute@users.noreply.github.com>
2025-07-21 08:46:01 +0300

    Modified tests of UserManagementService

--- src/test/java/com/coherentsolutions/pot/insurance_service/integration/InsuranceServiceApplicationTests.java
+++ src/test/java/com/coherentsolutions/pot/insurance_service/integration/InsuranceServiceApplicationTests.java
@@ -1,10 +1,10 @@
 package com.coherentsolutions.pot.insurance_service.integration;
 
+import com.coherentsolutions.pot.insurance_service.integration.containers.PostgresTestContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-import com.coherentsolutions.pot.insurance_service.integration.containers.PostgresTestContainer;
 
 @SpringBootTest
 @TestPropertySource(properties = "spring.main.allow-bean-definition-overriding=true")
