d4e577301d021582033a757fca5fef740be3f1bf
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-22 17:18:17 +0300

    Fix format issues

--- src/main/java/com/coherentsolutions/pot/insuranceservice/config/OpenApiConfig.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/config/OpenApiConfig.java
@@ -5,11 +5,18 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Configuration class for OpenAPI documentation in the Insurance Service application.
+ */
 @Configuration
 public class OpenApiConfig {
 
+  /**
+   * Defines the OpenAPI specification with custom title, version, and description for Swagger UI
+   * documentation.
+   */
   @Bean
-  public OpenAPI customOpenAPI() {
+  public OpenAPI customOpenApi() {
     return new OpenAPI()
         .info(new Info()
             .title("Insurance Service API")
