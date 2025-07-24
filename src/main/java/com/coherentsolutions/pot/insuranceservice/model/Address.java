a423057f5afa34378b77a37904ad987b3d9b0fc5
AgileAstapoviciute <148432756+AgileAstapoviciute@users.noreply.github.com>
2025-07-24 13:27:12 +0300

    Fix incorrect HTTP status code for invalid user input in User Controller (#86)
    * Fix incorrect HTTP status code for invalid user input in AdminUserManagementController
    
    * Added tests which were earlier removed by error
    
    * Added required fields to Address and Phone
    
    * Added address, phone list size validations, and email validation
    
    * Reverting formatting changes to be more readable
    
    * Added validation tests for update user and some more tests
    
    * fixed typo in UserDto.java

--- src/main/java/com/coherentsolutions/pot/insuranceservice/model/Address.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/model/Address.java
@@ -1,5 +1,6 @@
 package com.coherentsolutions.pot.insuranceservice.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,10 +16,19 @@ import lombok.Setter;
 @AllArgsConstructor
 public class Address {
 
+  @NotBlank(message = "Country is required")
   private String country;
+
+  @NotBlank(message = "City is required")
   private String city;
+
+  @NotBlank(message = "State is required")
   private String state;
+
+  @NotBlank(message = "Street is required")
   private String street;
+
+  @NotBlank(message = "Building is required")
   private String building;
   private String room;
 }
