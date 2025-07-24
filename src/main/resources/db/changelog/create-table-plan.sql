e1a3228bdc2d4f860e21cebe56edc39f85a5bdf1
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-24 11:08:46 +0300

    Add foreign key to users

--- src/main/resources/db/changelog/create-table-plan.sql
+++ src/main/resources/db/changelog/create-table-plan.sql
@@ -6,5 +6,8 @@ CREATE TABLE IF NOT EXISTS plans (
     created_by UUID,
     updated_by UUID,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_plan_created_by FOREIGN KEY (created_by) REFERENCES users(id),
+    CONSTRAINT fk_plan_updated_by FOREIGN KEY (updated_by) REFERENCES users(id)
 );
