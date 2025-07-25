5b2363791d11591e0e283c1a029947781e2f8474
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-25 16:38:46 +0300

    Add changeset for plan types

---
+++ src/main/resources/db/changelog/changeset/insert-plan-types.sql
@@ -0,0 +1,6 @@
+INSERT INTO plan_types (code, name)
+VALUES
+    ('DENTAL', 'Dental Insurance'),
+    ('MEDICAL', 'Medical Insurance'),
+    ('VISION', 'Vision Insurance'),
+    ('LIFE', 'Life Insurance');
