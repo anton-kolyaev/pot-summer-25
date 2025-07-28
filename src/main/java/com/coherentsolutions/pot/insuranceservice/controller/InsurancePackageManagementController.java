c1ce9d1e03c1703d1b65c3b9a2714d9ed67a6164
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-28 14:11:41 +0300

    Implement Post/plan-package

---
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/controller/InsurancePackageManagementController.java
@@ -0,0 +1,29 @@
+package com.coherentsolutions.pot.insuranceservice.controller;
+
+
+import com.coherentsolutions.pot.insuranceservice.dto.insurancepackage.InsurancePackageDto;
+import com.coherentsolutions.pot.insuranceservice.service.InsurancePackageManagementService;
+import jakarta.validation.Valid;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/company/{companyId}/plan-package")
+public class InsurancePackageManagementController {
+
+  private final InsurancePackageManagementService insurancePackageManagementService;
+
+  @PostMapping
+  public InsurancePackageDto createInsurancePackage(
+      @PathVariable UUID companyId,
+      @Valid @RequestBody InsurancePackageDto insurancePackageDto) {
+    return insurancePackageManagementService.createInsurancePackage(companyId, insurancePackageDto);
+  }
+
+}
