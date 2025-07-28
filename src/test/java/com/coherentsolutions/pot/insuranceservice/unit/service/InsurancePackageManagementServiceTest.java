6b678e6ce8b92c45dfb120d48365de6a3a05b5b9
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-28 18:34:41 +0300

    Add unit and integration tests

--- src/test/java/com/coherentsolutions/pot/insuranceservice/unit/service/InsurancePackageManagementServiceTest.java
+++ src/test/java/com/coherentsolutions/pot/insuranceservice/unit/service/InsurancePackageManagementServiceTest.java
@@ -3,15 +3,14 @@ package com.coherentsolutions.pot.insuranceservice.unit.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.coherentsolutions.pot.insuranceservice.dto.insurancepackage.InsurancePackageDto;
 import com.coherentsolutions.pot.insuranceservice.enums.PayrollFrequency;
 import com.coherentsolutions.pot.insuranceservice.mapper.InsurancePackageMapper;
-import com.coherentsolutions.pot.insuranceservice.model.Company;
 import com.coherentsolutions.pot.insuranceservice.model.InsurancePackage;
-import com.coherentsolutions.pot.insuranceservice.repository.CompanyRepository;
 import com.coherentsolutions.pot.insuranceservice.repository.InsurancePackageRepository;
 import com.coherentsolutions.pot.insuranceservice.service.InsurancePackageManagementService;
 import java.time.LocalDate;
@@ -22,6 +21,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("Insurance Packages Management Service Tests")
@@ -33,50 +34,56 @@ public class InsurancePackageManagementServiceTest {
   @Mock
   private InsurancePackageMapper insurancePackageMapper;
 
-  @Mock
-  private CompanyRepository companyRepository;
-
   @InjectMocks
   private InsurancePackageManagementService insurancePackageManagementService;
 
-
   @Test
-  @DisplayName("Should create insurance package and return it's dto")
-  void shouldCreateInsurancePackageSuccessfully() {
+  @DisplayName("Should get insurance package by it's id")
+  void shouldGetInsurancePackageById() throws Exception {
+    UUID packageId = UUID.randomUUID();
+
+    InsurancePackage insurancePackage = new InsurancePackage();
+    insurancePackage.setId(packageId);
+    insurancePackage.setName("Standard Health Package");
+    insurancePackage.setStartDate(LocalDate.of(2025, 8, 1));
+    insurancePackage.setEndDate(LocalDate.of(2025, 12, 31));
+    insurancePackage.setPayrollFrequency(PayrollFrequency.MONTHLY);
+
     InsurancePackageDto insurancePackageDto = InsurancePackageDto.builder()
+        .id(packageId)
         .name("Standard Health Package")
         .startDate(LocalDate.of(2025, 8, 1))
         .endDate(LocalDate.of(2025, 12, 31))
         .payrollFrequency(PayrollFrequency.MONTHLY)
         .build();
 
-    InsurancePackage insurancePackage = new InsurancePackage();
-    insurancePackage.setName(insurancePackageDto.getName());
-    insurancePackage.setStartDate(insurancePackageDto.getStartDate());
-    insurancePackage.setEndDate(insurancePackageDto.getEndDate());
-    insurancePackage.setPayrollFrequency(insurancePackageDto.getPayrollFrequency());
-
-    UUID companyId = UUID.randomUUID();
-
-    Company company = new Company();
-    company.setId(companyId);
-
-    when(insurancePackageMapper.toInsurancePackage(insurancePackageDto)).thenReturn(
-        insurancePackage);
-    when(companyRepository.findByIdOrThrow(companyId)).thenReturn(company);
-    when(insurancePackageRepository.save(insurancePackage)).thenReturn(insurancePackage);
+    when(insurancePackageRepository.findByIdOrThrow(packageId)).thenReturn(insurancePackage);
     when(insurancePackageMapper.toInsurancePackageDto(insurancePackage)).thenReturn(
         insurancePackageDto);
 
-    InsurancePackageDto result = insurancePackageManagementService.createInsurancePackage(companyId,
-        insurancePackageDto);
+    InsurancePackageDto result = insurancePackageManagementService.getInsurancePackageById(
+        packageId);
 
     assertNotNull(result);
     assertEquals(insurancePackageDto, result);
 
-    verify(insurancePackageMapper).toInsurancePackage(insurancePackageDto);
-    verify(insurancePackageRepository).save(insurancePackage);
+    verify(insurancePackageRepository).findByIdOrThrow(packageId);
     verify(insurancePackageMapper).toInsurancePackageDto(insurancePackage);
-    verify(companyRepository).findByIdOrThrow(companyId);
+  }
+
+  @Test
+  @DisplayName("Should throw Insurance package not found when trying to get non-existent package")
+  void shouldThrowInsurancePackageNotFound() {
+    UUID packageId = UUID.randomUUID();
+
+    when(insurancePackageRepository.findByIdOrThrow(packageId)).thenThrow(
+        new ResponseStatusException(HttpStatus.NOT_FOUND,
+            "Insurance package not found")
+    );
+
+    assertThrows(ResponseStatusException.class, () ->
+        insurancePackageManagementService.getInsurancePackageById(packageId));
+
+    verify(insurancePackageRepository).findByIdOrThrow(packageId);
   }
 }
