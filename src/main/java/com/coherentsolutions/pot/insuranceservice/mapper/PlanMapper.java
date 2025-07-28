fb372aec45e6327a36ae94f0df7818ac2fa57137
Kasparas3 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-29 00:46:24 +0300

    LJT-109 POST /plans (#94)
    * Implement POST /plans endpoint
    
    * Add changeset for plan types
    
    * Add tests for plans
    
    * Fix ConnectException error
    
    * Resolve PlanType by ID instead of code
    
    * Assign IDs to plan types
    
    * Remove unused autowired field
    
    * Remove unnecessary method
    
    * Undo not needed change
    
    * Prevent Spring from replacing PostgreSQL container with in-memory DB during tests

---
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/mapper/PlanMapper.java
@@ -0,0 +1,16 @@
+package com.coherentsolutions.pot.insuranceservice.mapper;
+
+import com.coherentsolutions.pot.insuranceservice.dto.plan.PlanDto;
+import com.coherentsolutions.pot.insuranceservice.model.Plan;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface PlanMapper {
+
+  @Mapping(source = "type.id", target = "type")
+  PlanDto toDto(Plan plan);
+
+  @Mapping(target = "type", ignore = true)
+  Plan toEntity(PlanDto planDto);
+}
