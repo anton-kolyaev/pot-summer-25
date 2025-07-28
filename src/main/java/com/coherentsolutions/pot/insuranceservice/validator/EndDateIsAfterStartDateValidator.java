799c9c361664714d536437183691a1699320b87b
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-28 14:10:45 +0300

    Update validator, make annotation usable for any objects

--- src/main/java/com/coherentsolutions/pot/insuranceservice/validator/EndDateIsAfterStartDateValidator.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/validator/EndDateIsAfterStartDateValidator.java
@@ -1,17 +1,39 @@
 package com.coherentsolutions.pot.insuranceservice.validator;
 
 import com.coherentsolutions.pot.insuranceservice.annotation.ValidateEndDateIsAfterStartDate;
-import com.coherentsolutions.pot.insuranceservice.dto.insurancepackage.InsurancePackageDto;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import java.lang.reflect.Method;
 import java.time.LocalDate;
 
 public class EndDateIsAfterStartDateValidator
-    implements ConstraintValidator<ValidateEndDateIsAfterStartDate, InsurancePackageDto> {
+    implements ConstraintValidator<ValidateEndDateIsAfterStartDate, Object> {
 
-  public boolean isValid(InsurancePackageDto insurancePackageDto,
-      ConstraintValidatorContext context) {
-    return insurancePackageDto.getEndDate().isAfter(LocalDate.now());
+  @Override
+  public boolean isValid(Object obj, ConstraintValidatorContext context) {
+    try {
+      Method getStartDate = obj.getClass().getMethod("getStartDate");
+      Method getEndDate = obj.getClass().getMethod("getEndDate");
+
+      Object startDateObj = getStartDate.invoke(obj);
+      Object endDateObj = getEndDate.invoke(obj);
+
+      LocalDate startDate = (LocalDate) startDateObj;
+      LocalDate endDate = (LocalDate) endDateObj;
+
+      if (startDate == null || endDate == null || !endDate.isAfter(startDate)) {
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(context.getDefaultConstraintMessageTemplate())
+            .addPropertyNode("endDate")
+            .addConstraintViolation();
+        return false;
+      }
+
+      return true;
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
   }
 
 }
