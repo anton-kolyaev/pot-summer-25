7ea9abff595b6ed270d9f09199b04ca8d20c88b9
Mark <mark.andzejevski01@gmail.com>
2025-07-28 15:40:54 +0300

    Save inbetween commit

--- src/main/java/com/coherentsolutions/pot/insuranceservice/config/OpenApiConfig.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/config/OpenApiConfig.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
   
   @Bean
-  public OpenAPI customOpenApi(@Value("${AUTH0_DOMAIN}") String domain) {
+  public OpenAPI customOpenApi(@Value("${AUTH0_DOMAIN:}") String domain) {
     SecurityScheme oauthScheme = new SecurityScheme()
             .type(SecurityScheme.Type.OAUTH2)
             .flows(new OAuthFlows().authorizationCode(
