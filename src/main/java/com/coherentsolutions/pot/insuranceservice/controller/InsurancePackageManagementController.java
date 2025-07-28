52079c88b244e22bfed0c9333bd7fee5a151a760
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-28 15:46:31 +0300

    Add integration tests

--- src/main/java/com/coherentsolutions/pot/insuranceservice/controller/InsurancePackageManagementController.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/controller/InsurancePackageManagementController.java
@@ -6,10 +6,12 @@ import com.coherentsolutions.pot.insuranceservice.service.InsurancePackageManage
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,6 +22,7 @@ public class InsurancePackageManagementController {
   private final InsurancePackageManagementService insurancePackageManagementService;
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public InsurancePackageDto createInsurancePackage(
       @PathVariable UUID companyId,
       @Valid @RequestBody InsurancePackageDto insurancePackageDto) {
--- src/main/java/com/coherentsolutions/pot/insuranceservice/repository/InsurancePackageRepository.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/repository/InsurancePackageRepository.java
@@ -1,11 +1,12 @@
 package com.coherentsolutions.pot.insuranceservice.repository;
 
 import com.coherentsolutions.pot.insuranceservice.model.InsurancePackage;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface InsurancePackageRepository extends JpaRepository<InsurancePackage, UUID> {
-
+  List<InsurancePackage> findByCompanyId(UUID companyId);
 }
