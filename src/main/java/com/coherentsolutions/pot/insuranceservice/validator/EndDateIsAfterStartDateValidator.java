a6c1789760e10cd3f4c8b2df02d478b0579fc45a
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-29 12:14:10 +0300

    Make annotation use param names

--- src/main/java/com/coherentsolutions/pot/insuranceservice/validator/EndDateIsAfterStartDateValidator.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/validator/EndDateIsAfterStartDateValidator.java
@@ -3,23 +3,30 @@ package com.coherentsolutions.pot.insuranceservice.validator;
 import com.coherentsolutions.pot.insuranceservice.annotation.ValidateEndDateIsAfterStartDate;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import java.lang.reflect.Method;
+import java.beans.PropertyDescriptor;
 import java.time.LocalDate;
 
 public class EndDateIsAfterStartDateValidator
     implements ConstraintValidator<ValidateEndDateIsAfterStartDate, Object> {
 
+  private String startDateField;
+  private String endDateField;
+
+  @Override
+  public void initialize(ValidateEndDateIsAfterStartDate constraintAnnotation) {
+    this.startDateField = constraintAnnotation.startDate();
+    this.endDateField = constraintAnnotation.endDate();
+  }
+
   @Override
   public boolean isValid(Object obj, ConstraintValidatorContext context) {
     try {
-      Method getStartDate = obj.getClass().getMethod("getStartDate");
-      Method getEndDate = obj.getClass().getMethod("getEndDate");
+      LocalDate startDate = (LocalDate) new PropertyDescriptor(startDateField, obj.getClass())
+          .getReadMethod().invoke(obj);
 
-      Object startDateObj = getStartDate.invoke(obj);
-      Object endDateObj = getEndDate.invoke(obj);
+      LocalDate endDate = (LocalDate) new PropertyDescriptor(endDateField, obj.getClass())
+          .getReadMethod().invoke(obj);
 
-      LocalDate startDate = (LocalDate) startDateObj;
-      LocalDate endDate = (LocalDate) endDateObj;
 
       if (startDate == null || endDate == null || !endDate.isAfter(startDate)) {
         context.disableDefaultConstraintViolation();
