79274769f91d9e8beebc063b3fa0304ff1905132
Mark <mark.andzejevski01@gmail.com>
2025-07-29 11:12:17 +0300

    Add Auditable

---
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/model/audit/Auditable.java
@@ -0,0 +1,21 @@
+package com.coherentsolutions.pot.insuranceservice.model.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+@Getter
+@MappedSuperclass
+public abstract class Auditable extends CreationAudit {
+  
+  @LastModifiedBy
+  @Column(name = "updated_by", nullable = false)
+  private String updatedBy;
+  
+  @LastModifiedDate
+  @Column(name = "updated_at", nullable = false)
+  private Instant updatedAt;
+}
