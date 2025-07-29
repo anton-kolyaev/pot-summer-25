dc9d9faa6f18376c3f4d5e6393b32b37d3b5bca0
Mark <mark.andzejevski01@gmail.com>
2025-07-29 11:11:07 +0300

    Add CreationAudit class

---
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/model/audit/CreationAudit.java
@@ -0,0 +1,22 @@
+package com.coherentsolutions.pot.insuranceservice.model.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+
+@Getter
+@MappedSuperclass
+public abstract class CreationAudit {
+  
+  @CreatedBy
+  @Column(name = "created_by", nullable = false, updatable = false)
+  private String createdBy;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private Instant createdAt;
+  
+}
