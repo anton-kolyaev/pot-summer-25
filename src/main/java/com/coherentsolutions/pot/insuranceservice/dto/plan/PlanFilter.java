900e8827c3c6fcc52e314ef733c7219731f41b3c
20233146 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-29 11:23:36 +0300

    LJT-108 Implement GET /plans endpoint

---
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/dto/plan/PlanFilter.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.pot.insuranceservice.dto.plan;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class PlanFilter {
+
+  private Integer typeId;
+}
