f267f18c4222c3d67da5b2fe7047bfd016a66638
Tishkin <nikita.tishkin@outlook.com>
2025-07-30 16:55:59 +0200

    removed getters and setters

--- src/main/java/com/coherentsolutions/pot/insuranceservice/dto/auth0/Auth0UserDto.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/dto/auth0/Auth0UserDto.java
@@ -5,6 +5,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * DTO for Auth0 user operations.
@@ -12,6 +15,9 @@ import java.util.Map;
  * <p>This DTO represents the data structure for creating and updating Auth0 users
  * through the backend API.
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Auth0UserDto {
 
   @JsonProperty("user_id")
@@ -48,101 +54,10 @@ public class Auth0UserDto {
   // Auth0 connection field - required for user creation
   private String connection = "Username-Password-Authentication";
 
-  // Constructors
-  public Auth0UserDto() {}
-
+  // Constructor for required fields
   public Auth0UserDto(String email, String password, String name) {
     this.email = email;
     this.password = password;
     this.name = name;
-  }
-
-  // Getters and Setters
-  public String getUserId() {
-    return userId;
-  }
-
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getNickname() {
-    return nickname;
-  }
-
-  public void setNickname(String nickname) {
-    this.nickname = nickname;
-  }
-
-  public String getPicture() {
-    return picture;
-  }
-
-  public void setPicture(String picture) {
-    this.picture = picture;
-  }
-
-  public Map<String, Object> getUserMetadata() {
-    return userMetadata;
-  }
-
-  public void setUserMetadata(Map<String, Object> userMetadata) {
-    this.userMetadata = userMetadata;
-  }
-
-  public Map<String, Object> getAppMetadata() {
-    return appMetadata;
-  }
-
-  public void setAppMetadata(Map<String, Object> appMetadata) {
-    this.appMetadata = appMetadata;
-  }
-
-  public boolean isEmailVerified() {
-    return emailVerified;
-  }
-
-  public void setEmailVerified(boolean emailVerified) {
-    this.emailVerified = emailVerified;
-  }
-
-  public boolean isBlocked() {
-    return blocked;
-  }
-
-  public void setBlocked(boolean blocked) {
-    this.blocked = blocked;
-  }
-
-  public String getConnection() {
-    return connection;
-  }
-
-  public void setConnection(String connection) {
-    this.connection = connection;
   }
 } 
