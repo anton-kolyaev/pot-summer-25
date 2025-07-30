e06b05eea863e51df79f619c1c6e123f68d4c346
Edgar Miloš <edgarmilos7@gmail.com>
2025-07-30 20:53:51 +0300

    Add a static method for package status calculation

--- src/main/java/com/coherentsolutions/pot/insuranceservice/service/InsurancePackageStatusUpdater.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/service/InsurancePackageStatusUpdater.java
@@ -16,25 +16,28 @@ public class InsurancePackageStatusUpdater {
 
   private final InsurancePackageRepository insurancePackageRepository;
 
+  public static PackageStatus calculateStatus(InsurancePackage insurancePackage) {
+    LocalDate now = LocalDate.now();
+    if (now.isBefore(insurancePackage.getStartDate())) {
+      return PackageStatus.INITIALIZED;
+    } else if (!now.isAfter(insurancePackage.getEndDate())) {
+      return PackageStatus.ACTIVE;
+    } else {
+      return PackageStatus.EXPIRED;
+    }
+  }
+
   @Scheduled(cron = "0 0 0 * * *")
   @Transactional
   public void updatePackageStatus() {
     List<InsurancePackage> packages = insurancePackageRepository.findAll();
 
     for (InsurancePackage insurancePackage : packages) {
-      if (insurancePackage.getStatus() != PackageStatus.DEACTIVATED) {
-        LocalDate now = LocalDate.now();
-        if (now.isBefore(insurancePackage.getStartDate())) {
-          insurancePackage.setStatus(PackageStatus.INITIALIZED);
-        } else if (!now.isAfter(insurancePackage.getEndDate())) {
-          insurancePackage.setStatus(PackageStatus.ACTIVE);
-        } else {
-          insurancePackage.setStatus(PackageStatus.EXPIRED);
-        }
-      }
+      insurancePackage.setStatus(calculateStatus(insurancePackage));
     }
 
     insurancePackageRepository.saveAll(packages);
   }
+
 
 }
