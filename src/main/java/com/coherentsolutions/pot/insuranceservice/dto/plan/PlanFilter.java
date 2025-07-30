b444573d9a0fa422dc94ac83a40d5e6048f8f9a6
Kasparas3 <kasparas.murmokas@stud.vilniustech.lt>
2025-07-30 10:45:20 +0300

    Remove @Getter and @Setter

--- src/main/java/com/coherentsolutions/pot/insuranceservice/dto/plan/PlanFilter.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/dto/plan/PlanFilter.java
@@ -1,11 +1,7 @@
 package com.coherentsolutions.pot.insuranceservice.dto.plan;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Data
 public class PlanFilter {
 
