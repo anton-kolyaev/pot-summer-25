9679dad4dd75ff7c6674c741f65faa81906cb077
Mark <mark.andzejevski01@gmail.com>
2025-08-08 10:40:49 +0300

    Add ClaimFilter

---
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/dto/claim/ClaimFilter.java
@@ -0,0 +1,22 @@
+package com.coherentsolutions.pot.insuranceservice.dto.claim;
+
+import com.coherentsolutions.pot.insuranceservice.enums.ClaimStatus;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ClaimFilter {
+
+  private String claimNumber;
+  private Integer number;
+  private ClaimStatus status;
+  private LocalDate serviceDateFrom;
+  private LocalDate serviceDateTo;
+  private UUID userId;
+  private UUID companyId;
+}
