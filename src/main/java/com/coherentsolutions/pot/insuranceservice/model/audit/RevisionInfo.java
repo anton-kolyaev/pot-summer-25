ff3be7195169daf60fddb536fd6dcf2a750c34c2
Mark <mark.andzejevski01@gmail.com>
2025-08-14 00:13:11 +0300

    Fix id generation errors

--- src/main/java/com/coherentsolutions/pot/insuranceservice/model/audit/RevisionInfo.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/model/audit/RevisionInfo.java
@@ -2,6 +2,8 @@ package com.coherentsolutions.pot.insuranceservice.model.audit;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
@@ -26,6 +28,7 @@ public class RevisionInfo {
 
   @Id
   @RevisionNumber
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "rev")
   private Integer id;
 
