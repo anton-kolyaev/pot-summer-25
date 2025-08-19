ee70d97522b25bdece4f3e8f0ae608b127c91c68
MarkAndz <114013967+MarkAndz@users.noreply.github.com>
2025-08-19 08:42:25 +0300

    Ljt 84 add auditing for users and companies (#121)
    * Add envers dependency
    
    * Add SecurityAuditor helper
    
    * Add RevisionInfoListener
    
    * Reformat AuditConfig
    
    * Add RevisionInfo
    
    * Add RevInfo table
    
    * Add companies and users aud tables
    
    * Audit User
    
    * Enable company auditing
    
    * Fix id generation errors
    
    * Add AuditHistoryService
    
    * Add AuditController
    
    * Fix endspace
    
    * Add envers it tests
    
    * Minor users_aud fix
    
    * Use lombok constructor AuditController
    
    * Apply max length to status var
    
    * Add logging to SecurityAuditor
    
    * Remove unused SYSTEM UUID
    
    * Refactor SecurityAuditor
    
    * Extract logger methods
    
    * Add unit tests
    
    * Fix AuditManagementControllerIt tests
    
    * Fix AuditConfigIt tests

--- src/main/java/com/coherentsolutions/pot/insuranceservice/config/AuditConfig.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/config/AuditConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.lang.NonNull;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
 @EnableJpaAuditing(auditorAwareRef = "auditorProvider")
@@ -19,22 +17,12 @@ public class AuditConfig {
     return new SecurityAuditorAware();
   }
 
-  private static final UUID SYSTEM = UUID.fromString("00000000-0000-0000-0000-000000000000");
-
   private static class SecurityAuditorAware implements AuditorAware<UUID> {
 
     @NonNull
     @Override
     public Optional<UUID> getCurrentAuditor() {
-      Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-
-      if (auth == null || !auth.isAuthenticated()) {
-        return Optional.of(SYSTEM);
-      }
-      // Branch is hit when the SecurityContext doesn’t contain a valid JWT-based Authentication,
-      // e.g. during scheduled jobs or anonymous/unauthenticated requests.
-      var authUuid = auth.getName() != null ? UUID.fromString(auth.getName()) : SYSTEM;
-      return Optional.of(authUuid);
+      return Optional.of(SecurityAuditor.currentUserOrSystem());
     }
   }
 }
