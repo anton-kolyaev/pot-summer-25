fedf9a8625fe9ad4ffce73f3bbb87641c9e44eec
Mark <mark.andzejevski01@gmail.com>
2025-08-21 00:25:10 +0300

    Add ClaimApprovalRequest

--- src/main/java/com/coherentsolutions/pot/insuranceservice/dto/claim/ClaimApprovalRequest.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/dto/claim/ClaimApprovalRequest.java
@@ -9,9 +9,9 @@ import lombok.Data;
 @Data
 public class ClaimApprovalRequest {
 
-  @NotNull(message = "approvedAmount is required")
-  @Positive(message = "approvedAmount must be positive")
-  @Digits(integer = 17, fraction = 2, message = "approvedAmount must have max 2 decimals")
+  @NotNull(message = "Approved amount is required")
+  @Positive(message = "Approved amount must be positive")
+  @Digits(integer = 17, fraction = 2, message = "Approved amount must have max 2 decimals")
   private BigDecimal approvedAmount;
 
   private String notes;
