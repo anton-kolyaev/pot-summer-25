ae503f0f3fae1c4ba9c1c7e91a8fe3254d449b33
Mark <mark.andzejevski01@gmail.com>
2025-08-20 20:09:04 +0300

    Added fields to changeset

--- src/main/resources/db/changelog/create-table-claims.sql
+++ src/main/resources/db/changelog/create-table-claims.sql
@@ -1,18 +1,22 @@
 CREATE TABLE IF NOT EXISTS claims
 (
-    id            UUID PRIMARY KEY        DEFAULT uuid_generate_v4(),
+    id              UUID PRIMARY KEY        DEFAULT uuid_generate_v4(),
 
-    claim_number  VARCHAR(100) NOT NULL,
-    status        VARCHAR(20)    NOT NULL DEFAULT 'PENDING'
-        CHECK (status IN ('PENDING', 'APPROVED', 'DECLINED', 'HOLD')),
-    service_date  DATE           NOT NULL,
-    user_id       UUID           NOT NULL,
-    enrollment_id UUID           NOT NULL,
-    amount        NUMERIC(19, 2) NOT NULL,
-    created_by    UUID,
-    updated_by    UUID,
-    created_at    TIMESTAMP               DEFAULT CURRENT_TIMESTAMP,
-    updated_at    TIMESTAMP               DEFAULT CURRENT_TIMESTAMP
+    claim_number    VARCHAR(100)   NOT NULL,
+    status          VARCHAR(20)    NOT NULL DEFAULT 'PENDING'
+        CHECK (status IN ('PENDING', 'APPROVED', 'DENIED', 'HOLD')),
+    service_date    DATE           NOT NULL,
+    processed_date  TIMESTAMP      NULL,
+    user_id         UUID           NOT NULL,
+    enrollment_id   UUID           NOT NULL,
+    amount          NUMERIC(19, 2) NOT NULL,
+    approved_amount NUMERIC(19, 2) NULL,
+    notes           TEXT           NULL,
+    denied_reason   TEXT           NULL,
+    created_by      UUID,
+    updated_by      UUID,
+    created_at      TIMESTAMP               DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP               DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE claims
