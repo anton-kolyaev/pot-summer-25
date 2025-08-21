c1e6cbc272687ceb6d5753f528622c2ca434a3f8
Edgar Miloš <edgarmilos7@gmail.com>
2025-08-21 03:14:54 +0300

    Fix getting a claim for company_id

--- src/main/java/com/coherentsolutions/pot/insuranceservice/security/service/CompanyAdminSecurityService.java
+++ src/main/java/com/coherentsolutions/pot/insuranceservice/security/service/CompanyAdminSecurityService.java
@@ -1,6 +1,7 @@
 package com.coherentsolutions.pot.insuranceservice.security.service;
 
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -22,7 +23,13 @@ public class CompanyAdminSecurityService {
     boolean belongsToCompany = checkIfBelongsToCompany(authentication, companyId);
     boolean hasRequiredRole = authentication.getAuthorities().stream()
         .anyMatch(a -> a.getAuthority().equals(requiredRole));
+    System.out.println(requiredRole);
 
+    authentication.getAuthorities().forEach(authority ->
+        System.out.println(authority.getAuthority())
+    );
+    System.out.println(hasRequiredRole);
+    System.out.println(belongsToCompany);
     return hasRequiredRole && belongsToCompany;
   }
 
@@ -31,9 +38,12 @@ public class CompanyAdminSecurityService {
         .anyMatch(a -> a.getAuthority().equals("ROLE_APPLICATION_ADMIN"));
   }
 
+  @Value("${AUTH0_AUDIENCE:}")
+  private String authAudience;
+
   private boolean checkIfBelongsToCompany(Authentication authentication, UUID companyId) {
     if (authentication.getPrincipal() instanceof Jwt jwt) {
-      String tokenCompanyId = jwt.getClaimAsString("company_id");
+      String tokenCompanyId = jwt.getClaimAsString(authAudience + "/company_id");
       return tokenCompanyId != null && tokenCompanyId.equals(companyId.toString());
     }
     return false;
